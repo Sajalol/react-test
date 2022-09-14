9a4d0cdc16ddd7f153f30b8ed60af58670287eb9
Stian Lystad <Stianlystad_1@hotmail.com>
2022-09-14 11:58:53 +0000

    challenge 3

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import FunctionalGreetingWithProps from "./components/FunctionalGreetingWithProps";
 import StatefulGreeting from "./components/StatefulGreeting";
 import HooksCounter from "./components/HooksCounter";
+import NavBarSimple from "./components/NavBarSimple";
 
 function App() {
   return (
-    <div className="App">
-
-      <StatefulGreeting greeting ="from Norway" name="Mike"/>
+    <div className>
+      {/* Add your components here */}
+      <NavBarSimple />
     </div>
   );
 }
