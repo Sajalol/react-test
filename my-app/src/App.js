9e8710bf87291d538fcc6d3409483bb822dc9bea
Stian Lystad <Stianlystad_1@hotmail.com>
2022-09-14 10:28:45 +0000

    finish class

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
 
-      <FunctionalGreetingWithProps greeting="Nice to meet you!" name="Mike" age="32" />
+      <StatefulGreeting greeting ="from Norway" name="Mike"/>
     </div>
   );
 }
