e2e24ee2ed482be05d3ae5870644c80b4b40974f
Stian Lystad <Stianlystad_1@hotmail.com>
2022-09-14 12:53:52 +0000

    handling events

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,14 +3,14 @@ import FunctionalGreetingWithProps from "./components/FunctionalGreetingWithProp
 import StatefulGreeting from "./components/StatefulGreeting";
 import HooksCounter from "./components/HooksCounter";
 import NavBarSimple from "./components/NavBarSimple";
+import EventsClass from './components/EventsClass';
+import EventsFunctional from './components/EventsFunctional';
 
 function App() {
-  return (
-    <div className>
-      {/* Add your components here */}
-      <NavBarSimple />
-    </div>
-  );
+  return <div className="App">
+    <EventsFunctional />
+    <EventsClass />
+  </div>
 }
 
 export default App;
---
+++ my-app/src/components/ES7Snippets.js
@@ -0,0 +1,55 @@
+// this is from rfce command
+
+// import React from 'react'
+
+// function ES7Snippets() {
+//   return (
+//    <div>ES7Snippets</div>
+//  )
+//}
+
+//export default ES7Snippets
+
+// this is from rfce command
+
+
+// This is from rafce command:
+import React from 'react'
+
+const ES7Snippets = () => {
+  return (
+    <div>ES7Snippets</div>
+  )
+}
+
+export default ES7Snippets
+
+// This is from rce command
+
+import React, { Component } from 'react'
+
+export class ES7Snippets extends Component {
+  render() {
+    return (
+      <div>ES7Snippets</div>
+    )
+  }
+}
+
+export default ES7Snippets
+
+// this is from rcon command
+constructor(props) {
+  super(props)
+
+  this.state = {
+     first
+  }
+}
+
+// console command: clg
+console.log(first)
+
+//clo command for console
+console.log('first', first)
+
